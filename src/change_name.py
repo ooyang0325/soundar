510c96c0d1e0e3db7c6e4ea453539e02b2650a0d
ooyang0325 <diego930325@gmail.com>
2022-11-22 01:53:44 +0800

    de-pre0 file names

--- src/change_name.py
+++ src/change_name.py
@@ -2,8 +2,7 @@ import os
 
 file_path = '../data/output/'
 
-for i in range(10):
-	file_name = file_path + 'output_' + '0' * 4 + str(i) + '.wav'
-	# os.rename(file_name, 'output_' + str(i))
-	os.rename('output_' + str(i), 'output_' + str(i) + '.wav')
+for i in range(1000, 10000):
+	file_name = file_path + 'output_' + '0' * 1 + str(i) + '.wav'
+	os.rename(file_name, file_path + 'output_' + str(i) + '.wav')
 
